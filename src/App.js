ae07faf505fa51f1a1fefebd7ab413c78b1970ee
Sheersh Jindal <sheershjindal@gmail.com>
2022-01-28 14:06:10 +0530

    Added basic user table feature

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Users from "./components/Users";
+import './css/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Users/>
+    </div>
+  );
+}
+
+export default App;
