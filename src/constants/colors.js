cbe3a0b30187be325c7a6ecd343384712e8d2753
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-01-30 13:33:56 +0530

    add color schema

---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+export default {
+    primary: "#7582EB",
+    success: "#14B8A6",
+    failure: "#D14343",
+    pending: "#FFB020",
+    background: "#0B0F19",
+    dark: "#111827",
+    light: "#1f2937",
+    highlight:"#10b981",
+    text: {
+        primary: "#ffffff",
+        secondary: "#A0AEC0"
+    }
+}
