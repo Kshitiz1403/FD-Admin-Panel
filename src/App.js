8827fed80f6a298efe05ec61b1e982bcd85d8c93
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-02-01 02:54:56 +0530

    fixed background color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Button from "./components/shared/Button";
 import Users from "./components/Users";
+import colors from "./constants/colors";
 import './css/App.css'
 
 function App() {
   const clickhandler = name => console.log("delete", name);
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:colors.background}}>
       <Users click = {clickhandler}/>
       {/* <Button text="Update" containerStyles={{}} textStyles={{}} empty onClick={()=>{}}/> */}
     </div>
